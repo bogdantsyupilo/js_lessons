270687ead47aaca64e5bf4aff06c909c75db1f34
Bogdan Tsyupilo <bogdantsyupilo@gmail.com>
2019-08-22 19:43:45 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Variables
+let money = 10;
+let income = 1000000;
+let addExpenses;
+let deposit;
+var mission;
+var period;
+
+// Alert + Console.log
+alert('No Money - No Honey :(');
+console.log(money + '$ - what we got, ' + income + '$ - what we need');
