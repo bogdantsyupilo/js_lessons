9c55fbed85e8445b63574881c4154be123958748
Bogdan Tsyupilo <bogdantsyupilo@gmail.com>
2019-10-01 18:15:58 +0300

    Lesson 19 Finished

--- main.js
+++ main.js
@@ -369,45 +369,31 @@ window.addEventListener('DOMContentLoaded', ()=> {
 
             }
             postData(body)
-                .then(outputData)
-                .catch(error => console.error(error))
+                .then((response)=> {
+                    if (response.status !== 200){
+                        throw new Error ('Status network: NOT 200');
+                    }
+                    statusMessage.textContent = successMessage; 
+                })
+                .catch((error)=> {
+                    statusMessage.textContent = errorMessage;
+                    console.error(error);
+                })
                 .then(resetForm);
         });
-
-        const outputData = ()=> {
-            statusMessage.textContent = successMessage;
-        };
-
-        const errorData = (error)=> {
-            statusMessage.textContent = errorMessage;
-            console.log(error);
-        };
 
         const resetForm = ()=> {
             form.reset();
         };
         
         const postData = (body)=> {
-            
-            return new Promise((resolve, reject)=> {
-                const request = new XMLHttpRequest();
 
-                request.addEventListener('readystatechange', ()=> {
-                    if(request.readyState !== 4) {
-                        return;
-                    } 
-
-                    if(request.status === 200) {
-                        resolve();
-                    } else {
-                        reject(request.status);
-                    }
-                });
-
-                request.open('POST', './server.php');
-                request.setRequestHeader('Content-Type', 'application/json');
-                request.send(JSON.stringify(body));
-                resetForm();
+            return fetch('./server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
             });
         };
     };
